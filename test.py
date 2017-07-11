eead3e35c571640dbd4e71c9d85613e957686c1c
subhadarshi samal <subh.samal15@gmail.com>
2017-07-11 00:36:28 -0700

    Adding problem-4 Calculate_handlen

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
-def normalize(numbers):
-    max_number = max(numbers)
-    for i in range(len(numbers)):
-        numbers[i] /= float(max_number)
-    return numbers
+points = 0
+total_points = 0
+while len(hand) > 0:
+    print('Current Hand: ', end='')
+    displayHand(hand)
+    word = input ("Enter word, or a '.' to indicate that you are finished: ")
+    if word == '.':
+        print ("Goodbye! Total score: %d  points." % total_points)
+        break
+    elif isValidWord(word, hand, wordList) == True :
+        points = getWordScore(word, n)
+        total_points += points
+        print ('" %s "' " earned %d points. Total: %d points " % (word, points, total_points))
+        print ()
+        if len(hand) == len(word):
+            print ("Run out of letters. Total score:  %d  points." % total_points)
+            break
+        else:
+            hand = updateHand(hand, word)
 
-
-
-
-try:
-      normalize([0, 0, 0])
-except ZeroDivisionError:
-      print('Invalid maximum element')
+    else:
+        print ("Invalid word, please try again.\n")
